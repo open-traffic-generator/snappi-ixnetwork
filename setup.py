fa1bdfc0b7a6547ff963cecade9126c9e6f959b2
Andy Balogh <andy.balogh@keysight.com>
2020-09-13 10:52:17 -0700

    fix distribution

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     include_package_data=True,
     python_requires='>=2.7, <4',
     install_requires=[
-        'jsonpath-ng'
+        'jsonpath-ng',
         'abstract-open-traffic-generator', 
         'ixnetwork-restpy'
     ],
