2a74bef8a875f4f3f512281012ac73baa7e01700
Andy Balogh <andy.balogh@keysight.com>
2020-09-13 22:19:07 -0700

    use api

--- tests/test_clear_config.py
+++ tests/test_clear_config.py
@@ -1,16 +1,9 @@
 import pytest
 
 
-def test_clear_config(serializer, tx_port):
+def test_clear_config(serializer, api, tx_port):
     """Demonstrates how to clear an IxNetwork session configuration
     """
-    from abstract_open_traffic_generator.config import Config
-    from ixnetwork_open_traffic_generator.ixnetworkapi import IxNetworkApi
-    
-    # set the ixnetwork connection parameters
-    api = IxNetworkApi('10.36.66.49', port=11009)
-
-    # set an empty configuration to clear the remote configuration 
     api.set_config(None)
     assert(len(api._assistant.Ixnetwork.Vport.find()) == 0)
     assert(len(api._assistant.Ixnetwork.Topology.find()) == 0)
