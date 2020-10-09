6b3fd087026792230ac39566a63cc4e6af056821
Andy Balogh <andy.balogh@keysight.com>
2020-10-09 10:48:27 -0700

    change imports

--- tests/test_ports.py
+++ tests/test_ports.py
@@ -1,17 +1,24 @@
 import pytest
-from abstract_open_traffic_generator.config import Config
-from abstract_open_traffic_generator.port import *
-from abstract_open_traffic_generator.control import *
+import abstract_open_traffic_generator.port as port
+import abstract_open_traffic_generator.config as config
+import abstract_open_traffic_generator.control as control
 
 
 def test_ports(serializer, api, options):
+    """Demonstrates adding ports to a configuration and setting the
+    configuration on the traffic generator.
+    The traffic generator should have no  items configured other than
+    the ports in this test.
+    """
     ports = [
-        Port(name='port1', location='10.36.74.26;01;01'),
-        Port(name='port2', location='10.36.77.102;12;03'),
-        Port(name='port no location')
+        port.Port(name='port1', location='10.36.74.26;01;01'),
+        port.Port(name='port2', location='10.36.77.102;12;03'),
+        port.Port(name='port no location')
     ]
-    config = Config(ports=ports, options=options)
-    api.set_state(State(ConfigState(config=config, state='set')))
+    configuration = config.Config(ports=ports, options=options)
+    state = control.State(control.ConfigState(config=configuration, state='set'))
+    api.set_state(state)
+
 
 if __name__ == '__main__':
     pytest.main(['-s', __file__])
