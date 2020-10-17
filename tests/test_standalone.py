b0215ef59a3acbb9480f087c8ae4061b0f2bc522
Andy Balogh <andy.balogh@keysight.com>
2020-10-16 17:01:32 -0700

    update readme with sample

--- tests/test_standalone.py
+++ tests/test_standalone.py
@@ -1,18 +1,3 @@
-# Keysight IxNetwork Open Traffic Generator
-[![Build](https://github.com/open-traffic-generator/ixnetwork/workflows/Build/badge.svg)](https://github.com/open-traffic-generator/ixnetwork/actions)
-[![pypi](https://img.shields.io/pypi/v/ixnetwork-open-traffic-generator.svg)](https://pypi.org/project/ixnetwork-open-traffic-generator)
-[![python](https://img.shields.io/pypi/pyversions/ixnetwork-open-traffic-generator.svg)](https://pypi.python.org/pypi/ixnetwork-open-traffic-generator)
-[![license](https://img.shields.io/badge/license-MIT-green.svg)](https://en.wikipedia.org/wiki/MIT_License)
-
-The Keysight IxNetwork implementation of the open-traffic-generator models.
-
-# Getting Started
-## Install the client
-```
-pip install --upgrade ixnetwork-open-traffic-generator
-```
-## Start scripting
-```python
 """A simple test that demonstrates the following:
 - Create 2 ports that transmit an ethernet/vlan/ipv4/tcp flow 
 for a specified duration. 
@@ -60,5 +45,3 @@ while True:
     print(df)
     if df.frames_tx.sum() >= config.flows[0].duration.packets.packets:
         break
-```
-
