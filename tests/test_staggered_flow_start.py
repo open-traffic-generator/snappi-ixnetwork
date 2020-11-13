e49033754d8f2e87c6f657b923adbf47afc836df
ajbalogh <andy.balogh@keysight.com>
2020-11-13 05:32:26 -0800

    add location preemption to test

--- tests/test_staggered_flow_start.py
+++ tests/test_staggered_flow_start.py
@@ -5,7 +5,7 @@ from abstract_open_traffic_generator.flow import *
 from abstract_open_traffic_generator.control import *
 
 
-def test_staggered_flow_start(serializer, api, tx_port, rx_port):
+def test_staggered_flow_start(serializer, api, options, tx_port, rx_port):
     """Demonstrates how to incrementally start transmit on flows
     """
     # configure flows
@@ -24,7 +24,8 @@ def test_staggered_flow_start(serializer, api, tx_port, rx_port):
                              tx_rx=TxRx(tx_rx),
                              rate=Rate('pps', value=1000),
                              duration=Duration(FixedPackets()))
-                    ])
+                    ],
+                    options=options)
     api.set_state(State(ConfigState(config=config, state='set')))
 
     # start flow1 transmit
