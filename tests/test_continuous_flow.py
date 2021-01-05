59f42d74a604ad8f4eb0562b803e3384a5385b69
Ashutosh Kumar <ashutshkumr@gmail.com>
2021-01-05 17:52:26 +0530

    change github actions runner to linux runner and IxN docker container

--- tests/test_continuous_flow.py
+++ tests/test_continuous_flow.py
@@ -1,16 +1,22 @@
 import pytest
-import time
 from abstract_open_traffic_generator.config import *
 from abstract_open_traffic_generator.flow import *
 from abstract_open_traffic_generator.control import *
+from abstract_open_traffic_generator import layer1
 
 
-def test_continuous_flow(serializer, api, options, tx_port, rx_port):
+def test_continuous_flow(serializer, api, options, tx_port, rx_port, settings):
     """Demonstrates how to create and start a continuous flow
     """
     # configure flows
     tx_rx = PortTxRx(tx_port_name=tx_port.name, rx_port_name=rx_port.name)
-    config = Config(ports=[tx_port, rx_port], options=options)
+    l1 = [
+        layer1.Layer1(
+            name='l1', port_names=[tx_port.name, rx_port.name],
+            speed=settings.speed, media=settings.media
+        )
+    ]
+    config = Config(ports=[tx_port, rx_port], layer1=l1, options=options)
     flow = Flow(name='Continuous Flow',
                 tx_rx=TxRx(tx_rx),
                 rate=Rate('pps', value=1000),
