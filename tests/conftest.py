22f8136987bf5cd2ab3048daa10fc15f7ff5f24a
ANISH-GOTTAPU <anish.gottapu@keysight.com>
2021-01-28 16:09:18 +0530

    updating conftest & utils for snappi

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,5 @@
 import pytest
-import json
-import yaml
-
+import snappi
 import utils as utl
 
 
@@ -30,258 +28,41 @@ def settings():
 @pytest.fixture(scope='session')
 def api():
     # handle to make API calls
-    api = utl.get_api_client()
+    api = snappi.api(host=utl.settings.api_server, ext=utl.settings.ext)
     yield api
+    # TODO: this is specific to IxNetwork
     if api.assistant is not None:
         api.assistant.Session.remove()
 
 
 @pytest.fixture
-def b2b_raw_config():
-    return utl.get_b2b_raw_config()
+def b2b_raw_config(api):
+    """
+    back to back raw config
+    """
+    config = api.config()
+
+    tx, rx = (
+        config.ports
+        .port(name='tx', location=utl.settings.ports[0])
+        .port(name='rx', location=utl.settings.ports[1])
+    )
+
+    l1 = config.layer1.layer1()[0]
+    l1.name = 'l1'
+    l1.port_names = [rx.name, tx.name]
+    l1.media = utl.settings.media
+
+    flow = config.flows.flow(name='f1')[-1]
+    flow.tx_rx.port.tx_name = tx.name
+    flow.tx_rx.port.rx_name = rx.name
+
+    # this will allow us to take over ports that may already be in use
+    config.options.port_options.location_preemption = True
+
+    return config
 
 
 @pytest.fixture
 def utils():
     return utl
-
-
-@pytest.fixture(scope='session')
-def serializer(request):
-    """Popular serialization methods
-    """
-    class Serializer(object):
-        def json(self, obj):
-            """Return a json string serialization of obj
-            """
-            import json
-            return json.dumps(obj, indent=2, default=lambda x: x.__dict__)
-
-        def yaml(self, obj):
-            """Return a yaml string serialization of obj
-            """
-            return yaml.dump(obj, indent=2)
-
-        def json_to_dict(self, json_string):
-            """Return a dict from a json string serialization
-            """
-            return json.loads(json_string)
-
-        def yaml_to_dict(self, yaml_string):
-            """Return a dict from a yaml string serialization
-            """
-            return yaml.load(yaml_string)
-
-    return Serializer()
-
-
-@pytest.fixture(scope='session')
-def options():
-    """Returns global options
-    """
-    from abstract_open_traffic_generator.config import Options
-    from abstract_open_traffic_generator.port import Options as PortOptions
-    return Options(PortOptions(location_preemption=True))
-
-
-@pytest.fixture(scope='session')
-def tx_port():
-    """Returns a transmit port
-    """
-    from abstract_open_traffic_generator.port import Port
-    return Port(name='Tx Port', location=utl.settings.ports[0])
-
-
-@pytest.fixture(scope='session')
-def rx_port():
-    """Returns a receive port
-    """
-    from abstract_open_traffic_generator.port import Port
-    return Port(name='Rx Port', location=utl.settings.ports[1])
-
-
-@pytest.fixture(scope='session')
-def b2b_devices(tx_port, rx_port):
-    """Returns a B2B tuple of tx devices and rx devices each with distinct device
-    groups of ethernet, ipv4, ipv6 and bgpv4 devices
-    """
-    from abstract_open_traffic_generator.device import Device, Ethernet, Vlan
-    from abstract_open_traffic_generator.device import Ipv4, Ipv6, Bgpv4
-    from abstract_open_traffic_generator.device import Pattern
-
-    devices = [
-        Device(name='Tx Devices Eth',
-               container_name=tx_port.name,
-               device_count=1,
-               choice=Ethernet(name='Tx Eth',
-                               vlans=[Vlan(name='Tx Eth Vlan')])),
-        Device(name='Tx Devices Ipv4',
-               container_name=tx_port.name,
-               device_count=2,
-               choice=Ipv4(name='Tx Ipv4',
-                           address=Pattern('1.1.1.1'),
-                           prefix=Pattern('24'),
-                           gateway=Pattern('1.1.2.1'),
-                           ethernet=Ethernet(name='Tx Ipv4 Eth',
-                                             vlans=[Vlan(name='Tx Ipv4 Vlan')
-                                                    ]))),
-        Device(name='Tx Devices Ipv6',
-               container_name=tx_port.name,
-               device_count=3,
-               choice=Ipv6(name='Tx Ipv6',
-                           ethernet=Ethernet(name='Tx Ipv6 Eth',
-                                             vlans=[Vlan(name='Tx Ipv6 Vlan')
-                                                    ]))),
-        Device(name='Tx Devices Bgpv4',
-               container_name=tx_port.name,
-               device_count=10,
-               choice=Bgpv4(name='Tx Bgpv4',
-                            ipv4=Ipv4(name='Tx Bgpv4 Ipv4',
-                                      ethernet=Ethernet(
-                                          name='Tx Bgpv4 Eth',
-                                          vlans=[Vlan(name='Tx Bgpv4 Vlan')
-                                                 ])))),
-        Device(name='Rx Devices Eth',
-               container_name=rx_port.name,
-               device_count=1,
-               choice=Ethernet(name='Rx Eth',
-                               vlans=[Vlan(name='Rx Eth Vlan')])),
-        Device(name='Rx Devices Ipv4',
-               container_name=rx_port.name,
-               device_count=2,
-               choice=Ipv4(name='Rx Ipv4',
-                           address=Pattern('1.1.1.1'),
-                           prefix=Pattern('24'),
-                           gateway=Pattern('1.1.2.1'),
-                           ethernet=Ethernet(name='Rx Ipv4 Eth',
-                                             vlans=[Vlan(name='Rx Ipv4 Vlan')
-                                                    ]))),
-        Device(name='Rx Devices Ipv6',
-               container_name=rx_port.name,
-               device_count=3,
-               choice=Ipv6(name='Rx Ipv6',
-                           ethernet=Ethernet(name='Rx Ipv6 Eth',
-                                             vlans=[Vlan(name='Rx Ipv6 Vlan')
-                                                    ]))),
-        Device(name='Rx Devices Bgpv4',
-               container_name=rx_port.name,
-               device_count=10,
-               choice=Bgpv4(name='Rx Bgpv4',
-                            ipv4=Ipv4(name='Rx Bgpv4 Ipv4',
-                                      ethernet=Ethernet(
-                                          name='Rx Bgpv4 Eth',
-                                          vlans=[Vlan(name='Rx Bgpv4 Vlan')
-                                                 ]))))
-    ]
-    return devices
-
-
-@pytest.fixture(scope='session')
-def b2b_ipv4_devices(tx_port, rx_port):
-    """Returns a list of ipv4 tx device and rx device objects
-    Each device object is ipv4, ethernet and vlan
-    """
-    from abstract_open_traffic_generator.device import Device, Ethernet, Vlan, Ipv4
-    from abstract_open_traffic_generator.device import Pattern
-
-    tx_device = Device(name='Tx Devices Ipv4',
-                       container_name=tx_port.name,
-                       device_count=1,
-                       choice=Ipv4(name='Tx Ipv4',
-                                   address=Pattern('1.1.1.1'),
-                                   prefix=Pattern('24'),
-                                   gateway=Pattern('1.1.2.1'),
-                                   ethernet=Ethernet(
-                                       name='Tx Ipv4 Eth',
-                                       vlans=[Vlan(name='Tx Ipv4 Vlan')])))
-
-    rx_device = Device(name='Rx Devices Ipv4',
-                       container_name=rx_port.name,
-                       device_count=1,
-                       choice=Ipv4(name='Rx Ipv4',
-                                   address=Pattern('1.1.2.1'),
-                                   prefix=Pattern('24'),
-                                   gateway=Pattern('1.1.1.1'),
-                                   ethernet=Ethernet(
-                                       name='Rx Ipv4 Eth',
-                                       vlans=[Vlan(name='Rx Ipv4 Vlan')])))
-
-    return [tx_device, rx_device]
-
-
-@pytest.fixture(scope='session')
-def b2b_port_flow_config(options, tx_port, rx_port):
-    """Returns a configuration with an ethernet flow.
-    The flow uses a PortTxRx endpoint.
-    """
-    from abstract_open_traffic_generator.config import Config
-    from abstract_open_traffic_generator.flow import Flow, TxRx, PortTxRx, \
-        Size, Rate, Duration, FixedPackets, Header, Ethernet
-
-    endpoint = PortTxRx(tx_port_name=tx_port.name, rx_port_name=rx_port.name)
-    flow = Flow(name='Port Flow',
-                tx_rx=TxRx(endpoint),
-                packet=[Header(Ethernet())],
-                size=Size(128),
-                rate=Rate(unit='pps', value=1000),
-                duration=Duration(FixedPackets(packets=10000)))
-    return Config(ports=[tx_port, rx_port], flows=[flow], options=options)
-
-
-@pytest.fixture(scope='session')
-def b2b_ipv4_flow_config(options, tx_port, rx_port, b2b_ipv4_devices):
-    """Returns a configuration with an ipv4 flow.
-    The flow uses a DeviceTxRx endpoint.
-    """
-    from abstract_open_traffic_generator.config import Config
-    from abstract_open_traffic_generator.flow import Flow, TxRx, DeviceTxRx, \
-        Size, Rate, Duration, FixedPackets, Header, Ethernet, Vlan, Ipv4
-
-    names = [device.name for device in b2b_ipv4_devices]
-    tx_rx_devices = DeviceTxRx(tx_device_names=names, rx_device_names=names)
-    flow = Flow(name='Ipv4 Flow',
-                tx_rx=TxRx(tx_rx_devices),
-                packet=[Header(Ethernet()),
-                        Header(Vlan()),
-                        Header(Ipv4())],
-                size=Size(512),
-                rate=Rate(unit='pps', value=1000),
-                duration=Duration(FixedPackets(10000)))
-    return Config(ports=[tx_port, rx_port],
-                  devices=b2b_ipv4_devices,
-                  flows=[flow],
-                  options=options)
-
-
-@pytest.fixture(scope='session')
-def b2b_ipv4_flows_config(options, tx_port, rx_port, b2b_ipv4_devices):
-    """Returns a configuration with multiple ipv4 flows.
-    The flow uses a DeviceTxRx endpoint.
-    """
-    from abstract_open_traffic_generator.config import Config
-    from abstract_open_traffic_generator.flow import Flow, TxRx, DeviceTxRx, \
-        Size, Rate, Duration, FixedPackets, Header, Ethernet, Vlan, Ipv4
-
-    names = [device.name for device in b2b_ipv4_devices]
-    tx_rx_devices = DeviceTxRx(tx_device_names=names, rx_device_names=names)
-    flow1 = Flow(name='Ipv4 Flow 1',
-                 tx_rx=TxRx(tx_rx_devices),
-                 packet=[Header(Ethernet()),
-                         Header(Vlan()),
-                         Header(Ipv4())],
-                 size=Size(512),
-                 rate=Rate(unit='pps', value=1000),
-                 duration=Duration(FixedPackets(10000)))
-
-    flow2 = Flow(name='Ipv4 Flow 2',
-                 tx_rx=TxRx(tx_rx_devices),
-                 packet=[Header(Ethernet()),
-                         Header(Vlan()),
-                         Header(Ipv4())],
-                 size=Size(512),
-                 rate=Rate(unit='pps', value=1000),
-                 duration=Duration(FixedPackets(10000)))
-    return Config(ports=[tx_port, rx_port],
-                  devices=b2b_ipv4_devices,
-                  flows=[flow1, flow2],
-                  options=options)
--- tests/utils/common.py
+++ tests/utils/common.py
@@ -1,16 +1,9 @@
-import csv
 import json
 import os
-import time
-import io
-import dpkt
 import sys
+import time
 from datetime import datetime
-
-# from abstract_open_traffic_generator import (
-#     config, port, layer1, flow, control, result, capture
-# )
-from snappi_ixnetwork.ixnetworkapi import Api
+import csv
 
 
 if sys.version_info[0] >= 3:
@@ -29,7 +22,9 @@ def get_root_dir():
 
 
 def get_test_config_path(config_name):
-    return os.path.join(get_root_dir(), CONFIGS_DIR, config_name)
+    return os.path.join(
+        os.path.dirname(get_root_dir()), CONFIGS_DIR, config_name
+    )
 
 
 def dict_items(d):
@@ -81,6 +76,7 @@ class Settings(object):
         self.log_level = None
         self.dynamic_stats_output = None
         self.license_servers = None
+        self.ext = None
 
         self.load_from_settings_file()
 
@@ -114,155 +110,28 @@ class Settings(object):
 settings = Settings()
 
 
-def get_api_client():
+def start_traffic(api, cfg):
     """
-    Returns api client configured according to global settings
-    """
-    host, port = settings.api_server.split(':')
-    return IxNetworkApi(
-        host, port=port, license_servers=[], log_level='info'
-    )
-
-
-def get_b2b_raw_config():
-    """
-    Returns raw b2b traffic config patched with ports and speed from global
-    settings and one default flow
-    """
-    ports = []
-    # modify config to use port locations provided in settings.json
-    for i, n in enumerate(['raw_tx', 'raw_rx']):
-        ports.append(port.Port(name=n, location=settings.ports[i]))
-
-    l1 = None
-    # modify speed
-    if settings.speed is not None:
-        l1 = [
-            layer1.Layer1(
-                name='l1', port_names=[p.name for p in ports],
-                speed=settings.speed, media=settings.media
-            )
-        ]
-
-    c = capture.Capture(
-        name='c1', port_names=[ports[1].name], enable=True,
-        choice=[], format='pcap'
-    )
-
-    # default flow with first two ports as tx/rx
-    f = flow.Flow(
-        name='f1',
-        tx_rx=flow.TxRx(
-            flow.PortTxRx(
-                tx_port_name=ports[0].name,
-                rx_port_name=ports[1].name
-            )
-        )
-    )
-
-    # forcefully take ownership of ports owned by other users
-    o = config.Options(port.Options(location_preemption=True))
-
-    return config.Config(
-        ports=ports, layer1=l1, flows=[f], captures=[c], options=o
-    )
-
-
-def get_capture_port_names(cfg):
-    """
-    Returns name of ports for which capture is enabled.
-    """
-    names = []
-    if cfg.captures:
-        for cap in cfg.captures:
-            if cap.port_names and cap.enable:
-                for name in cap.port_names:
-                    if name not in names:
-                        names.append(name)
-
-    return names
-
-
-def start_traffic(api, cfg, start_capture=True):
-    """
-    Applies configuration, starts capture and starts flows.
+    Applies configuration, and starts flows.
     """
     print('Setting config ...')
-    api.set_state(control.State(control.ConfigState(config=cfg, state='set')))
-
-    capture_names = get_capture_port_names(cfg)
-    if capture_names and start_capture:
-        print('Starting capture on ports %s ...' % str(capture_names))
-        api.set_state(
-            control.State(
-                control.PortCaptureState(
-                    port_names=capture_names, state='start'
-                )
-            )
-        )
+    api.set_config(cfg)
 
     print('Starting transmit on all flows ...')
-    api.set_state(control.State(control.FlowTransmitState(state='start')))
+    transmit_state = api.transmit_state()
+    transmit_state.state = 'start'
+    api.set_transmit_state(transmit_state)
 
 
-def stop_traffic(api, cfg, stop_capture=True):
+def stop_traffic(api):
     """
-    Stops flows and capture.
+    Stops flows
     """
-    print('Stopping transmit on all flows ...')
-    api.set_state(control.State(control.FlowTransmitState(state='stop')))
-    capture_names = get_capture_port_names(cfg)
-    if capture_names and stop_capture:
-        print('Stopping capture on ports %s ...' % str(capture_names))
-        api.set_state(
-            control.State(
-                control.PortCaptureState(
-                    port_names=capture_names, state='stop'
-                )
-            )
-        )
 
-
-def get_all_stats(api, print_output=True):
-    """
-    Returns all port and flow stats
-    """
-    print('Fetching all port stats ...')
-    port_results = api.get_port_results(result.PortRequest())
-    if port_results is None:
-        port_results = []
-
-    print('Fetching all flow stats ...')
-    flow_results = api.get_flow_results(result.FlowRequest())
-    if flow_results is None:
-        flow_results = []
-
-    if print_output:
-        print_stats(port_stats=port_results, flow_stats=flow_results)
-
-    return port_results, flow_results
-
-
-def get_all_captures(api, cfg):
-    """
-    Returns a dictionary where port name is the key and value is a list of
-    frames where each frame is represented as a list of bytes.
-    """
-    cap_dict = {}
-    for name in get_capture_port_names(cfg):
-        print('Fetching captures from port %s' % name)
-        pcap_bytes = api.get_capture_results(
-            result.CaptureRequest(port_name=name)
-        )
-
-        cap_dict[name] = []
-        for ts, pkt in dpkt.pcap.Reader(io.BytesIO(pcap_bytes)):
-            if sys.version_info[0] == 2:
-                cap_dict[name].append([ord(b) for b in pkt])
-            else:
-                cap_dict[name].append(list(pkt))
-
-    return cap_dict
+    print('Starting transmit on all flows ...')
+    transmit_state = api.transmit_state()
+    transmit_state.state = 'stop'
+    api.set_transmit_state(transmit_state)
 
 
 def seconds_elapsed(start_seconds):
@@ -278,7 +147,6 @@ def wait_for(func, condition_str, interval_seconds=None, timeout_seconds=None):
     Keeps calling the `func` until it returns true or `timeout_seconds` occurs
     every `interval_seconds`. `condition_str` should be a constant string
     implying the actual condition being tested.
-
     Usage
     -----
     If we wanted to poll for current seconds to be divisible by `n`, we would
@@ -287,10 +155,8 @@ def wait_for(func, condition_str, interval_seconds=None, timeout_seconds=None):
     import time
     def wait_for_seconds(n, **kwargs):
         condition_str = 'seconds to be divisible by %d' % n
-
         def condition_satisfied():
             return int(time.time()) % n == 0
-
         poll_until(condition_satisfied, condition_str, **kwargs)
     ```
     """
@@ -313,6 +179,44 @@ def wait_for(func, condition_str, interval_seconds=None, timeout_seconds=None):
             raise Exception(msg)
 
         time.sleep(interval_seconds)
+
+
+def get_all_stats(api, print_output=True):
+    """
+    Returns all port and flow stats
+    """
+    print('Fetching all port stats ...')
+    port_results_request = api.port_metrics_request()
+    port_results = api.get_port_metrics(port_results_request)
+    if port_results is None:
+        port_results = []
+
+    print('Fetching all flow stats ...')
+    flow_results_request = api.flow_metrics_request()
+    flow_results = api.get_flow_metrics(flow_results_request)
+    if flow_results is None:
+        flow_results = []
+
+    if print_output:
+        print_stats(port_stats=port_results, flow_stats=flow_results)
+
+    return port_results, flow_results
+
+
+def total_frames_ok(port_results, flow_results, expected):
+    port_tx = sum([p.frames_tx for p in port_results])
+    port_rx = sum([p.frames_rx for p in port_results])
+    flow_rx = sum([f.frames_rx for f in flow_results])
+
+    return port_tx == port_rx == flow_rx == expected
+
+
+def total_bytes_ok(port_results, flow_results, expected):
+    port_tx = sum([p.bytes_tx for p in port_results])
+    port_rx = sum([p.bytes_rx for p in port_results])
+    flow_rx = sum([f.bytes_rx for f in flow_results])
+
+    return port_tx == port_rx == flow_rx == expected
 
 
 def new_logs_dir(prefix='logs'):
@@ -366,8 +270,8 @@ def print_stats(port_stats=None, flow_stats=None, clear_screen=None):
         for stat in port_stats:
             print(
                 row_format.format(
-                    stat['name'], stat['frames_tx'], stat['bytes_tx'],
-                    stat['frames_rx'], stat['bytes_rx'], stat['frames_tx_rate']
+                    stat.name, stat.frames_tx, stat.bytes_tx,
+                    stat.frames_rx, stat.bytes_rx, stat.frames_tx_rate
                 )
             )
         print(border)
@@ -383,181 +287,12 @@ def print_stats(port_stats=None, flow_stats=None, clear_screen=None):
         for stat in flow_stats:
             print(
                 row_format.format(
-                    stat['name'], stat['frames_rx'], stat['bytes_rx']
+                    stat.name, stat.frames_rx, stat.bytes_rx
                 )
             )
         print(border)
         print("")
         print("")
-
-
-def flow_transmit_matches(flow_results, state):
-    return len(flow_results) == all(
-        [f['transmit'] == state for f in flow_results]
-    )
-
-
-def total_frames_ok(port_results, flow_results, expected):
-    port_tx = sum([p['frames_tx'] for p in port_results])
-    port_rx = sum([p['frames_rx'] for p in port_results])
-    flow_rx = sum([f['frames_rx'] for f in flow_results])
-
-    return port_tx == port_rx == flow_rx == expected
-
-
-def total_bytes_ok(port_results, flow_results, expected):
-    port_tx = sum([p['bytes_tx'] for p in port_results])
-    port_rx = sum([p['bytes_rx'] for p in port_results])
-    flow_rx = sum([f['bytes_rx'] for f in flow_results])
-
-    return port_tx == port_rx == flow_rx == expected
-
-
-def total_bytes_in_frame_size_range(start, end, step, count):
-    """
-    Returns total bytes after summing up all frame sizes in the given range.
-    """
-    pkt_count = 0
-    total_bytes = 0
-    partial_bytes = 0
-    for i in range(start, end + 1, step):
-        total_bytes += i
-        pkt_count += 1
-    total_count = int(count / pkt_count)
-    partial_count = count % pkt_count
-    n = 0
-    for i in range(start, end + 1, step):
-        if n == partial_count:
-            break
-        partial_bytes += i
-        n += 1
-
-    return total_bytes * total_count + partial_bytes
-
-
-def to_hex(lst):
-    """
-    Takes lst of data from packet capture and converts to hex
-
-    Ex: [11,184] is converted to 0xbb8
-        [0,30] is converted to 0x1e
-    """
-    from functools import reduce
-    value = reduce(lambda x, y: hex(x) + hex(y), lst)
-    value = value[0:2] + value[2:].replace("0x", "").lstrip("0")
-    return value
-
-
-def stats_ok(api, size, packets):
-    """
-    Returns true if stats are as expected, false otherwise.
-    """
-    port_results, flow_results = get_all_stats(api)
-
-    frames_ok = total_frames_ok(port_results, flow_results, packets)
-    bytes_ok = total_bytes_ok(port_results, flow_results, packets * size)
-    ok = frames_ok and bytes_ok
-
-    if flow_transmit_matches(flow_results, 'stopped') and not ok:
-        raise Exception('Stats not ok after flows are stopped')
-
-    return ok
-
-
-def is_traffic_stopped(api, flow_names=[]):
-    """
-    Returns true if traffic in stop state
-    """
-    flow_results = api.get_flow_results(result.FlowRequest(
-                                        flow_names=flow_names))
-    return all([f['transmit'] == 'stopped' for f in flow_results])
-
-
-def is_stats_accumulated(api, packets):
-    """
-    Returns true if stats gets accumulated
-    """
-    port_results, flow_results = get_all_stats(api)
-    frames_ok = total_frames_ok(port_results, flow_results, packets)
-    return frames_ok
-
-
-def value_list_with_packet_count(value_list, packet_count):
-    """
-    Example:
-        value_list_with_packet_count(['10.1.1.1', '10.1.1.3'], 6)
-        returns: ['10.1.1.1', '10.1.1.3', '10.1.1.1', '10.1.1.3',
-                  '10.1.1.1', '10.1.1.3']
-    """
-    ret_value = value_list * packet_count
-    return ret_value[:packet_count]
-
-
-def mac_or_ip_to_num(mac_or_ip_addr, mac=True):
-    """
-    Example:
-    mac_or_ip_to_num('00:0C:29:E3:53:EA')
-    returns: 52242371562
-    mac_or_ip_to_num('10.1.1.1', False)
-    returns: 167837953
-    """
-    sep = ':' if mac else '.'
-    addr = []
-    if mac:
-        addr = mac_or_ip_addr.split(sep)
-    else:
-        addr = ["{:02x}".format(int(i)) for i in mac_or_ip_addr.split(sep)]
-    return int("".join(addr), 16)
-
-
-def num_to_mac_or_ip(mac_or_ip_addr, mac=True):
-    """
-    Example:
-    num_to_mac_or_ip(52242371562)
-    returns: '00:0C:29:E3:53:EA'
-    num_to_mac_or_ip(167837953, False)
-    returns: '10.1.1.1'
-    """
-    sep = ':' if mac else '.'
-    fmt = '{:012x}' if mac else '{:08x}'
-    rng = 12 if mac else 8
-    mac_or_ip = fmt.format(mac_or_ip_addr)
-    addr = []
-    for i in range(0, rng, 2):
-        if mac:
-            addr.append(mac_or_ip[i] + mac_or_ip[i + 1])
-        else:
-            addr.append(str(int(mac_or_ip[i] + mac_or_ip[i + 1], 16)))
-    return sep.join(addr)
-
-
-def mac_or_ip_addr_from_counter_pattern(start_addr, step, count, up, mac=True):
-    """
-    Example:
-    mac_or_ip_addr_from_counter_pattern('10.1.1.1', '0.0.1.1', 2, True, False)
-    returns: ['00:0C:29:E3:53:EA', '00:0C:29:E3:54:EA']
-    mac_or_ip_addr_from_counter_pattern('10.1.1.1', '0.0.1.1', 2, True, False)
-    teturns: ['10.1.1.1', '10.1.2.2']
-    """
-    addr_list = []
-    for num in range(count):
-        addr_list.append(start_addr)
-        if up:
-            start_addr = mac_or_ip_to_num(
-                start_addr, mac) + mac_or_ip_to_num(step, mac)
-        else:
-            start_addr = mac_or_ip_to_num(
-                start_addr, mac) - mac_or_ip_to_num(step, mac)
-        start_addr = num_to_mac_or_ip(start_addr, mac)
-    return addr_list
-
-
-def apply_config(api, cfg):
-    """
-    Applies configuration, starts capture and starts flows.
-    """
-    print('Setting config ...')
-    api.set_state(control.State(control.ConfigState(config=cfg, state='set')))
 
 
 def get_value(field):
@@ -580,7 +315,6 @@ def get_packet_information(api, packet_header):
     Takes any packet_header or header position
     for ex ethernet, ipv4, udp, tcp and returns
     the packet information of that header
-
     if string is passed the header is filtered by name
     if int is passed header is filtered by index
     """
