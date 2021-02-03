bd71e2595376ecaa1a208c5485255bd357649658
Ashutosh Kumar <ashutshkumr@gmail.com>
2021-02-03 22:55:36 +0530

    bump snappi version to include latest model changes

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     ],
     extras_require={
         'dev': [
-            'snappi==0.1.27',
+            'snappi==0.1.32',
             'pytest',
             'flake8==3.8.4',
             'dpkt==1.9.4',
