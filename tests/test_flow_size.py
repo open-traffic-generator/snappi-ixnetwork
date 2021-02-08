6f482a7bca7a258f17bcf430be2c44c376353b70
ANISH-GOTTAPU <anish.gottapu@keysight.com>
2021-02-08 18:27:28 +0530

    updating snappi unit tests

--- tests/test_flow_size.py
+++ tests/test_flow_size.py
@@ -1,8 +1,7 @@
 import pytest
 
 
-@pytest.mark.skip("skip until migrated to snappi")
-def test_flow_sizes(serializer, api, tx_port, rx_port, b2b_ipv4_devices):
+def test_flow_sizes(api, settings):
     """
     This will test supported Flow Size
             'float': 'fixed',
@@ -10,41 +9,57 @@ def test_flow_sizes(serializer, api, tx_port, rx_port, b2b_ipv4_devices):
             'SizeIncrement': 'increment',
             'SizeRandom': 'random',
     """
-    port_endpoint = PortTxRx(tx_port_name=tx_port.name,
-                             rx_port_name=rx_port.name)
-    pause = Header(
-        PfcPause(dst=Pattern('01:80:C2:00:00:01'),
-                 class_enable_vector=Pattern('1'),
-                 pause_class_0=Pattern('1')))
-    fixed_size = Flow(name='Fixed Size',
-                      tx_rx=TxRx(port_endpoint),
-                      packet=[pause],
-                      size=Size(44),
-                      rate=Rate('line', value=100),
-                      duration=Duration(FixedPackets(packets=0)))
+    config = api.config()
 
-    increment = SizeIncrement(start=100, end=1200, step=10)
-    increment_size = Flow(name='Increment Size',
-                          tx_rx=TxRx(port_endpoint),
-                          packet=[pause],
-                          size=Size(increment),
-                          rate=Rate('line', value=100),
-                          duration=Duration(FixedPackets(packets=0)))
+    tx, rx = (
+        config.ports
+        .port(name='tx', location=settings.ports[0])
+        .port(name='rx', location=settings.ports[1])
+    )
 
-    random = SizeRandom()
-    random_size = Flow(name='Random Size',
-                       tx_rx=TxRx(port_endpoint),
-                       packet=[pause],
-                       size=Size(random),
-                       rate=Rate('line', value=100),
-                       duration=Duration(FixedPackets(packets=0)))
+    l1 = config.layer1.layer1()[0]
+    l1.name = 'l1'
+    l1.port_names = [rx.name, tx.name]
+    l1.media = settings.media
 
-    config = Config(ports=[tx_port, rx_port],
-                    devices=b2b_ipv4_devices,
-                    flows=[fixed_size, increment_size, random_size])
-    state = control.State(
-        control.ConfigState(config=config, state='set'))
-    api.set_state(state)
+    fixed_size, increment_size, random_size = (
+        config.flows
+        .flow(name='Fixed Size')
+        .flow(name='Increment Size')
+        .flow(name='Random Size')
+    )
+
+    fixed_size.tx_rx.port.tx_name = tx.name
+    fixed_size.tx_rx.port.rx_name = rx.name
+    pfc, = fixed_size.packet.pfcpause()
+    pfc.src.value = '00:AB:BC:AB:BC:AB'
+    pfc.dst.value = '00:AB:BC:AB:BC:AB'
+    pfc.ether_type.value = '8100'
+    pfc.class_enable_vector.value = 'FF'
+    pfc.control_op_code.value = '0101'
+    pfc.pause_class_0.value = 'FFFF'
+    pfc.pause_class_1.value = 'FFFF'
+    pfc.pause_class_2.value = 'FFFF'
+    pfc.pause_class_3.value = 'FFFF'
+    pfc.pause_class_4.value = 'FFFF'
+    pfc.pause_class_5.value = 'FFFF'
+    pfc.pause_class_6.value = 'FFFF'
+    pfc.pause_class_7.value = 'FFFF'
+    fixed_size.size.fixed = "44"
+
+    increment_size.tx_rx.port.tx_name = tx.name
+    increment_size.tx_rx.port.rx_name = rx.name
+    increment_size.size.increment.start = "100"
+    increment_size.size.increment.step = "10"
+    increment_size.size.increment.end = "1200"
+
+    random_size.tx_rx.port.tx_name = tx.name
+    random_size.tx_rx.port.rx_name = rx.name
+    random_size.size.random.min = "72"
+    random_size.size.random.max = "1518"
+
+    response = api.set_config(config)
+    assert(len(response.errors)) == 0
 
 
 if __name__ == '__main__':
