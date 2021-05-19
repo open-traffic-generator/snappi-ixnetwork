9988cf198eb5ae56c3dca6ca3f2f7c39b99fee0f
Ashutosh Kumar <ashutshkumr@gmail.com>
2021-05-19 12:38:40 +0530

    version 0.3.11

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     ],
     extras_require={
         'dev': [
-            'snappi==0.3.19',
+            'snappi==0.3.20',
             'pytest',
             'flake8==3.8.4',
             'dpkt==1.9.4',
