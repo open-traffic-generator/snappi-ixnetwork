c1fa7614f0fd1c0212b9523a90d7323b589c06e8
Automation <anish.gottapu@keysight.com>
2021-06-30 18:42:13 +0530

    add pretest

--- do.py
+++ do.py
@@ -36,6 +36,15 @@ def lint():
 
 
 def test():
+    pretest_args = [
+        '--location="https://10.39.71.97:443"',
+        (
+            '--ports="10.39.65.230;6;1 10.39.65.230;6;2 10.39.65.230;6;3'
+            ' 10.39.65.230;6;4"'
+        ),
+        '--media="fiber"',
+        "tests/test_pretest.py",
+    ]
     args = [
         '--location="https://10.39.71.97:443"',
         (
@@ -44,10 +53,11 @@ def test():
         ),
         '--media="fiber"',
         "tests",
-        '-m "not e2e and not l1_manual"',
+        '-m "not e2e and not l1_manual and not pretest"',
     ]
     run(
         [
+            py() + " -m pytest -sv {}".format(" ".join(pretest_args)),
             py() + " -m pytest -sv {}".format(" ".join(args)),
         ]
     )
---
+++ tests/test_pretest.py
@@ -0,0 +1,25 @@
+import pytest
+
+
+@pytest.mark.pretest
+def test_pretest(api, utils):
+    """
+    This test does 2 things
+    1) Restore ports to factory defaults and keep them ready for tests
+    2) Remove any previous stale sessions in API server
+
+    """
+    config = api.config()
+
+    for i in range(len(utils.settings.ports)):
+        config.ports.port(name="port%d" % i, location=utils.settings.ports[i])[
+            -1
+        ]
+
+    config.options.port_options.location_preemption = True
+    api.set_config(config)
+
+    # ports cleanup
+    api._ixnetwork.Vport.find().ResetPortCpuAndFactoryDefault()
+    # stale sessions cleanup
+    api.assistant.TestPlatform.Sessions.find().remove()
