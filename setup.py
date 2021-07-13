1d7477c0cb007bd246ed9b94b39e66d186761fc4
Alakendu Jana <alakendu.jana@keysight.com>
2021-07-13 15:10:58 +0530

    snappi==0.4.19

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setuptools.setup(
     install_requires=["ixnetwork-restpy>=1.0.52"],
     extras_require={
         "testing": [
-            "snappi==0.4.15",
+            "snappi==0.4.19",
             "snappi_convergence==0.0.18",
             "pytest",
             "dpkt==1.9.4",
