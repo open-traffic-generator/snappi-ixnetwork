2dab459ce40af0ea2870078364a028658eea1a70
ANISH-GOTTAPU <48308607+ANISH-GOTTAPU@users.noreply.github.com>
2021-09-02 12:53:47 +0530

    update do.py to change CI/CD to 100G
    update do.py to change CI/CD to 100G

--- do.py
+++ do.py
@@ -58,6 +58,7 @@ def test():
         ),
         "--ext=ixnetwork",
         "--speed=speed_100_gbps",
+        "tests",
         '-m "not e2e and not l1_manual"',
         '--cov=./snappi_ixnetwork --cov-report term --cov-report html:cov_report',
     ]
