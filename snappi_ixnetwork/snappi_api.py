ef56910042559e40f94337b74c9716ee5920e46f
alakjana <alakendu.jana@keysight.com>
2021-11-29 10:27:49 +0530

    add all len check

--- snappi_ixnetwork/snappi_api.py
+++ snappi_ixnetwork/snappi_api.py
@@ -612,12 +612,16 @@ class Api(snappi.Api):
                     ip4_list = eth_list.Ipv4.find()
                     ip6_list = eth_list.Ipv6.find()
                     if len(eth_list) == max(len(ip4_list), len(ip6_list)):
-                        ip4_list.Start(async_operation=True)
-                        ip6_list.Start(async_operation=True)
+                        if len(ip4_list) > 0:
+                            ip4_list.Start(async_operation=True)
+                        if len(ip6_list) > 0:
+                            ip6_list.Start(async_operation=True)
                     else:
                         eth_list.Start(async_operation=True)
-                        ip4_list.Start(async_operation=True)
-                        ip6_list.Start(async_operation=True)
+                        if len(ip4_list) > 0:
+                            ip4_list.Start(async_operation=True)
+                        if len(ip6_list) > 0:
+                            ip6_list.Start(async_operation=True)
 
     def _request(self, method, url, payload=None):
         connection, url = self._assistant.Session._connection._normalize_url(
