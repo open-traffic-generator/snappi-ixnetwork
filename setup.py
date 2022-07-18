c7e281a866e8f8beb040e6f3bc6ed5936dd42540
ANISH-GOTTAPU <48308607+ANISH-GOTTAPU@users.noreply.github.com>
2022-07-18 14:22:29 +0530

    Version 0.8.2
    Version 0.8.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 import setuptools
 
 pkg_name = "snappi_ixnetwork"
-version = "0.8.1"
+version = "0.8.2"
 
 # read long description from readme.md
 base_dir = os.path.abspath(os.path.dirname(__file__))
