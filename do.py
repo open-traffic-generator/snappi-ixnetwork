7dc2b3a9fe6f5c96b045e3ac543b589bc12994e3
vhowdhur <vibaswan.roychowdhury@keysight.com>
2023-07-26 19:53:49 +0530

    trying to make the password generic

--- do.py
+++ do.py
@@ -38,6 +38,7 @@ def lint():
 def test():
     coverage_threshold = 67
     username = os.environ.get("TEST_USERNAME", "admin")
+    psd = os.environ.get("TEST_PASSWORD", "admin")
     args = [
         '--location="https://snappi-ixn-ci-novus100g.lbj.is.keysight.com:5000"',
         (
@@ -47,9 +48,10 @@ def test():
             ' snappi-ixn-ci-novus100g.lbj.is.keysight.com;1;6"'
         ),
         "--username=" + username,
+        "--psd='" + psd,
         "--ext=ixnetwork",
         "--speed=speed_100_gbps",
-        "tests",
+        "tests\\ping\\test_ping.py",
         '-m "not e2e and not l1_manual and not uhd"',
         "--cov=./snappi_ixnetwork --cov-report term"
         " --cov-report html:cov_report",
