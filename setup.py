989ca49a91a61774a63084dd24d45a124b9e66ce
indraniBh <indrani.bhattacharya@keysight.com>
2024-08-30 07:57:04 +0000

    snappi 0.11.17

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 import setuptools
 
 pkg_name = "snappi_ixnetwork"
-version = "0.11.7"
+version = "0.11.17"
 
 # read long description from readme.md
 base_dir = os.path.abspath(os.path.dirname(__file__))
@@ -37,7 +37,7 @@ setuptools.setup(
     install_requires=["ixnetwork-restpy>=1.0.52"],
     extras_require={
         "testing": [
-            "snappi==0.11.7",
+            "snappi==0.11.17",
             # "snappi_convergence==0.4.1",
             "pytest",
             "mock",
