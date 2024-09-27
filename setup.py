5b7ae847ccb6859c46c315c95ce34d890e8e438a
indraniBh <indrani.bhattacharya@keysight.com>
2024-09-27 10:35:46 +0000

    remover snappi convergence ref in setup

--- setup.py
+++ setup.py
@@ -38,7 +38,6 @@ setuptools.setup(
     extras_require={
         "testing": [
             "snappi==1.13.0",
-            # "snappi_convergence==0.4.1",
             "pytest",
             "mock",
             "dpkt==1.9.4",
