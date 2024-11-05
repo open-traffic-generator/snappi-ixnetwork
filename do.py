54e1bf40bd5e639f2003ff557e48e2aa5fcf32ee
desaimg1 <desai.mg@keysight.com>
2024-11-05 17:54:48 +0530

    commit

--- do.py
+++ do.py
@@ -110,7 +110,7 @@ def generate_allure_report():
         run(["sudo -S apt-get update"])
         run(["sudo -S apt-get install default-jre"])
         run(["wget https://github.com/allure-framework/allure2/releases/tag/2.32.0/allure_2.32.0-1_all.deb"])
-        run(["sudo dpkg -i allure_2.18.1-1_all.deb"])
+        run(["sudo -S dpkg -i allure_2.18.1-1_all.deb"])
 
         run(
         [
