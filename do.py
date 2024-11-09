0d114d6834ca3cfa7b4bb4ad964be3d18fbf6577
desaimg1 <desai.mg@keysight.com>
2024-11-09 14:55:41 +0530

    run all the tests

--- do.py
+++ do.py
@@ -74,8 +74,8 @@ def test(card="novus100g"):
         "--username=" + username,
         "--psd='" + psd + "'",
         "tests",
-        #'-m "not e2e and not l1_manual and not uhd"',
-        '-m "runonly"',
+        '-m "not e2e and not l1_manual and not uhd"',
+        #'-m "runonly"',
         "--cov=./snappi_ixnetwork --cov-report term",
         " --cov-report html:cov_report",
         " -o junit_logging=all --junitxml=allure-results/report-pytest.xml"
