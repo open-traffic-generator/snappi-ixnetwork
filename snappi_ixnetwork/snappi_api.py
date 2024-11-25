60ebfe97a927192611a3be1a1f69db14655d3b9a
indraniBh <indrani.bhattacharya@keysight.com>
2024-11-25 06:13:03 +0000

    Added lag and lacp support in get_metrics

--- snappi_ixnetwork/snappi_api.py
+++ snappi_ixnetwork/snappi_api.py
@@ -620,6 +620,16 @@ class Api(snappi.Api):
                 metric_res = self.metrics_response()
                 metric_res.flow_metrics.deserialize(response)
                 return metric_res
+            if request.get("choice") == "lag":
+                response = self.traffic_item.results(request.lag)
+                metric_res = self.metrics_response()
+                metric_res.lag_metrics.deserialize(response)
+                return metric_res
+            if request.get("choice") == "lacp":
+                response = self.traffic_item.results(request.lacp)
+                metric_res = self.metrics_response()
+                metric_res.lacp_metrics.deserialize(response)
+                return metric_res
             if (
                 request.get("choice")
                 in self.protocol_metrics.get_supported_protocols()
