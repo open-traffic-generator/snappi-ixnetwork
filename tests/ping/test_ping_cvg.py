a73ab54f6f350c20c23d4d952a6e1063cdc9ed49
itachary <indrani.bhattacharya@keysight.com>
2025-01-13 12:19:21 +0000

    Convergence metrics added + TCs

--- tests/ping/test_ping_cvg.py
+++ tests/ping/test_ping_cvg.py
@@ -1,20 +1,19 @@
 import pytest
 
-@pytest.mark.skip(reason="Fix -convergence support TBD")
-def test_ping_cvg(cvg_api, utils):
+
+def test_ping_cvg(api, utils):
     """
     Demonstrates test to send ipv4 and ipv6 pings
 
     Return the ping responses and validate as per user's expectation
     """
-    conv_config = cvg_api.convergence_config()
-    config = conv_config.config
-    port1, port2 = config.ports.port(
+    conv_config = api.config()
+    port1, port2 = conv_config.ports.port(
         name="tx", location=utils.settings.ports[0]
     ).port(name="rx", location=utils.settings.ports[1])
 
-    config.options.port_options.location_preemption = True
-    ly = config.layer1.layer1()[-1]
+    conv_config.options.port_options.location_preemption = True
+    ly = conv_config.layer1.layer1()[-1]
     ly.name = "ly"
     ly.port_names = [port1.name, port2.name]
     ly.ieee_media_defaults = False
@@ -22,9 +21,9 @@ def test_ping_cvg(cvg_api, utils):
     ly.speed = utils.settings.speed
     ly.media = utils.settings.media
 
-    d1, d2 = config.devices.device(name="tx_bgp").device(name="rx_bgp")
+    d1, d2 = conv_config.devices.device(name="tx_bgp").device(name="rx_bgp")
     eth1, eth2 = d1.ethernets.add(), d2.ethernets.add()
-    eth1.port_name, eth2.port_name = port1.name, port2.name
+    eth1.connection.port_name, eth2.connection.port_name = port1.name, port2.name   # noqa
     eth1.mac, eth2.mac = "00:00:00:00:00:11", "00:00:00:00:00:22"
     ip1, ip2 = eth1.ipv4_addresses.add(), eth2.ipv4_addresses.add()
     ipv61, ipv62 = eth1.ipv6_addresses.add(), eth2.ipv6_addresses.add()
@@ -48,44 +47,55 @@ def test_ping_cvg(cvg_api, utils):
     ipv62.gateway = "3000::1"
     ipv62.prefix = 64
 
-    cvg_api.set_config(conv_config)
+    api.set_config(conv_config)
 
     print("Starting all protocols ...")
-    cs = cvg_api.convergence_state()
-    cs.protocol.state = cs.protocol.START
-    cvg_api.set_state(cs)
+    ps = api.control_state()
+    ps.choice = ps.PROTOCOL
+    ps.protocol.choice = ps.protocol.ALL
+    ps.protocol.all.state = ps.protocol.all.START
+    res = api.set_control_state(ps)
+    if len(res.warnings) > 0:
+        print("Warnings: {}".format(res.warnings))
 
-    cs = cvg_api.convergence_state()
-    cs.transmit.state = cs.transmit.START
-    cvg_api.set_state(cs)
+    cs = api.control_state()
+    cs.choice = cs.TRAFFIC
+    cs.traffic.choice = cs.traffic.FLOW_TRANSMIT
+    cs.traffic.flow_transmit.state = cs.traffic.flow_transmit.START
+    res = api.set_control_state(cs)
+    if len(res.warnings) > 0:
+        print("Warnings: {}".format(res.warnings))
 
     try:
-        req = cvg_api.ping_request()
-        p1, p2, p3, p4 = req.endpoints.ipv4().ipv4().ipv6().ipv6()
-        p1.src_name = ip1.name
-        p1.dst_ip = "10.1.1.2"
-        p2.src_name = ip1.name
-        p2.dst_ip = "10.1.1.3"
-        p3.src_name = ipv62.name
-        p3.dst_ip = "3000::1"
-        p4.src_name = ipv62.name
-        p4.dst_ip = "3000::9"
+        cs = api.control_action()
+        cs.protocol.ipv4.ping.requests.add(src_name=ip1.name, dst_ip="10.1.1.2")
+        cs.protocol.ipv4.ping.requests.add(src_name=ip1.name, dst_ip="10.1.1.3")
 
-        responses = cvg_api.send_ping(req).responses
+        responses = api.set_control_action(
+            cs
+        ).response.protocol.ipv4.ping.responses
         for resp in responses:
             if resp.src_name == ip1.name and resp.dst_ip == "10.1.1.2":
-                assert resp.result == "success"
+                assert resp.result == "succeeded"
             elif resp.src_name == ip1.name and resp.dst_ip == "10.1.1.3":
-                assert resp.result == "failure"
+                assert resp.result == "failed"
             elif resp.src_name == ipv62.name and resp.dst_ip == "3000::1":
-                assert resp.result == "success"
+                assert resp.result == "succeeded"
             elif resp.src_name == ipv62.name and resp.dst_ip == "3000::9":
-                assert resp.result == "failure"
-        cs = cvg_api.convergence_state()
-        cs.transmit.state = cs.transmit.STOP
-        cvg_api.set_state(cs)
+                assert resp.result == "failed"
+        cs = api.control_state()
+        cs.choice = cs.TRAFFIC
+        cs.traffic.choice = cs.traffic.FLOW_TRANSMIT
+        cs.traffic.flow_transmit.state = cs.traffic.flow_transmit.STOP
+        res = api.set_control_state(cs)
+        if len(res.warnings) > 0:
+            print("Warnings: {}".format(res.warnings))
     except Exception as e:
-        cs = cvg_api.convergence_state()
-        cs.transmit.state = cs.transmit.STOP
-        cvg_api.set_state(cs)
+        cs = api.control_state()
+        cs.choice = cs.TRAFFIC
+        cs.traffic.choice = cs.traffic.FLOW_TRANSMIT
+        cs.traffic.flow_transmit.state = cs.traffic.flow_transmit.STOP
+        res = api.set_control_state(cs)
+        if len(res.warnings) > 0:
+            print("Warnings: {}".format(res.warnings))
         raise Exception(e)
