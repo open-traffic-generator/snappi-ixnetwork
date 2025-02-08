19d0b0f167836dc18261b731fb2388e02ca88ae3
Subrata Sa <subrata.sa@keysight.com>
2025-02-08 18:29:58 +0530

    Add MKA support for OTG to IxNetwork. Also update test script

--- tests/mka/test_mka.py
+++ tests/mka/test_mka.py
@@ -11,14 +11,14 @@ def test_mka(api, b2b_raw_config, utils):
     b2b_raw_config.flows.clear()
 
     p1, p2 = b2b_raw_config.ports
-    d1, d2 = b2b_raw_config.devices.device(name="mka1").device(name="mka2")
+    d1, d2 = b2b_raw_config.devices.device(name="mka_dev1").device(name="mka_dev2")
 
     eth1, eth2 = d1.ethernets.add(), d2.ethernets.add()
     eth1.connection.port_name, eth2.connection.port_name = p1.name, p2.name
     eth1.mac, eth2.mac = "00:00:00:00:00:11", "00:00:00:00:00:22"
     eth1.name, eth2.name = "eth1", "eth2"
 
-    mka1, mka = d1.mka, d2.mka
+    mka1, mka2 = d1.mka, d2.mka
     mka1_int, mka2_int = mka1.ethernet_interfaces.add(), mka2.ethernet_interfaces.add()
     mka1_int.eth_name, mka2_int.eth_name = eth1.name, eth2.name
     kay1, kay2 = mka1_int.kay, mka2_int.kay
@@ -31,13 +31,14 @@ def test_mka(api, b2b_raw_config, utils):
 
     # Tx SC
     kay1_txsc1, kay2_txsc1 = kay1.txscs.add(), kay2.txscs.add() 
+    kay1_txsc1.name, kay2_txsc1.name = "txsc1", "txsc2"
     kay1_txsc1.system_id, kay2_txsc1.system_id = eth1.mac, eth2.mac 
     # Remaining Tx SC settings autofilled
 
     utils.start_traffic(api, b2b_raw_config)
-    print("Sleeping for 10 secoonds: start")
+    print("Sleeping for 30 secoonds: start")
     time.sleep(30)
-    print("Sleeping for 10 secoonds: end")
+    print("Sleeping for 30 secoonds: end")
     utils.stop_traffic(api, b2b_raw_config)
 
 
