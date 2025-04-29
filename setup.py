75346cc91b1863bfdb7a0be8586dce1d36f0f64d
Subrata Sa <subrata.sa@keysight.com>
2025-04-29 11:10:47 +0530

    Update setup.py to run CI with desired snappi==1.30.0

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setuptools.setup(
     install_requires=["ixnetwork-restpy>=1.0.52"],
     extras_require={
         "testing": [
-            "snappi==1.27.1",
+            "snappi==1.30.0",
             "pytest",
             "mock",
             "dpkt==1.9.4",
