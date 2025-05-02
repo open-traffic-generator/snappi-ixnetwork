4ff0aed0cccaf7ee25bbee80ab2c2175bfcd4217
itachary <indrani.bhattacharya@keysight.com>
2025-05-02 16:05:00 +0000

    Testing CI: Restructuring code

--- do.py
+++ do.py
@@ -52,6 +52,7 @@ def lint():
 
 def test(card="novus100g"):
     coverage_threshold = 20
+    sanity_threshold = 100
     username = os.environ.get("TEST_USERNAME", "admin")
     psd = os.environ.get("TEST_PASSWORD", "admin")
 
@@ -118,6 +119,48 @@ def test(card="novus100g"):
                     coverage_threshold, result[idx]
                 )
             )
+    select_tc, passed_tc, failed_tc, skip_test = extract_log()
+    pass_rate = (int(passed_tc) / (int(failed_tc) + int(passed_tc))) * 100
+    print(pass_rate)
+    if int(pass_rate) < sanity_threshold:
+        raise Exception(
+                "Sanity thresold[{0}] is NOT achieved[{1}]".format(
+                    sanity_threshold, pass_rate
+                )
+        )
+    else:
+        print(
+            "Sanity thresold[{0}] is achieved[{1}]".format(
+                sanity_threshold, pass_rate
+            )
+        )
+
+
+def extract_log():
+    with open("myfile.log") as fp:
+        out = fp.read()
+        total_selected_tests = re.findall(r"collected.*\s+(\d+)\s+items", out)[0]
+        print("total_selected", total_selected_tests)
+        total_passed_tests = re.findall(r"=.*\s(\d+)\s+passed", out)[0]
+        print("total_passed", total_passed_tests)
+        if re.findall(r"=.*\s(\d+)\s+skipped",out):
+            total_skipped_tests = re.findall(r"=.*\s(\d+)\s+skipped", out)[0]
+            print("total_skipped", total_skipped_tests)
+        else:
+            total_skipped_tests = 0
+        
+        total_failed_tests = int(total_selected_tests) - int(total_passed_tests) - int(total_skipped_tests)
+        print("total_failed_tests", total_failed_tests)
+
+    val1 = total_selected_tests
+    val2 = total_passed_tests
+    val3 = total_failed_tests
+    pass_rate = (int(val2) / (int(val2) + int(val3))) * 100
+    val4 = pass_rate
+    val5 = total_skipped_tests
+
+    return val1, val2, val3, val4, val5
+
     
 def generate_allure_report():
         run(["mkdir -p allure-results/history"])
@@ -153,23 +196,24 @@ def generate_allure_report():
 def coverage_mail():
 
     test_start = (subprocess.check_output("echo $TIMESTAMP", shell=True)).decode('ascii')
-    coverage_threshold = 67
     global result
-    with open("myfile.log") as fp:
-        out = fp.read()
-        total_selected_tests = re.findall(r"collected.*\s+(\d+)\s+items", out)[0]
-        print("total_selected", total_selected_tests)
-        total_passed_tests = re.findall(r"=.*\s(\d+)\s+passed", out)[0]
-        print("total_passed", total_passed_tests)
-        if re.findall(r"=.*\s(\d+)\s+skipped",out):
-            total_skipped_tests = re.findall(r"=.*\s(\d+)\s+skipped", out)[0]
-            print("total_skipped", total_skipped_tests)
-        else:
-            total_skipped_tests = 0
+    # with open("myfile.log") as fp:
+    #     out = fp.read()
+    #     total_selected_tests = re.findall(r"collected.*\s+(\d+)\s+items", out)[0]
+    #     print("total_selected", total_selected_tests)
+    #     total_passed_tests = re.findall(r"=.*\s(\d+)\s+passed", out)[0]
+    #     print("total_passed", total_passed_tests)
+    #     if re.findall(r"=.*\s(\d+)\s+skipped",out):
+    #         total_skipped_tests = re.findall(r"=.*\s(\d+)\s+skipped", out)[0]
+    #         print("total_skipped", total_skipped_tests)
+    #     else:
+    #         total_skipped_tests = 0
         
-        total_failed_tests = int(total_selected_tests) - int(total_passed_tests) - int(total_skipped_tests)
-        print("total_failed_tests", total_failed_tests)
+    #     total_failed_tests = int(total_selected_tests) - int(total_passed_tests) - int(total_skipped_tests)
+    #     print("total_failed_tests", total_failed_tests)
 
+    select_tc, passed_tc, failed_tc, skip_test = extract_log()
+    pass_rate = (int(passed_tc) / (int(failed_tc) + int(passed_tc))) * 100
     with open("./cov_report/index.html") as fp:
         out = fp.read()
         result = re.findall(r"data-ratio.*?[>](\d+)\b", out)[-1]
@@ -182,9 +226,12 @@ def coverage_mail():
     msg['From'] = sender
     msg['To'] = ", ".join(receiver)
 
-    val1=total_selected_tests
-    val2=total_passed_tests
-    val3=total_failed_tests
+    # val1=total_selected_tests
+    # val2=total_passed_tests
+    # val3=total_failed_tests
+    val1 = select_tc
+    val2 = passed_tc
+    val3 = failed_tc
 
     build_number=get_workflow_id()
 
