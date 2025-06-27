a4b63d48f0de0ff0e100522b4df5e6f377039932
itachary <indrani.bhattacharya@keysight.com>
2025-06-27 00:48:23 +0000

    WIP:Added interfaces and router

--- snappi_ixnetwork/device/isis.py
+++ snappi_ixnetwork/device/isis.py
@@ -3,6 +3,45 @@ from snappi_ixnetwork.logger import get_ixnet_logger
 
 
 class Isis(Base):
+    _ISIS = {
+        "system_id": "systemId",
+    }
+
+    _NETWORK_TYPE = {
+        "network_type": {
+            "ixn_attr": "networkType",
+            "enum_map": {"broadcast": "broadcast", "point_to_point": "pointpoint"}, # noqa
+        },
+    }
+
+    _LEVEL_TYPE = {
+        "level_type": {
+            "ixn_attr": "levelType",
+            "enum_map": {"level_1": "level1", "level_2": "level2", "level_1_2": "l1l2"}, # noqa
+        },
+    }
+
+    _AUTH_TYPE = {
+        "auth_type": {
+            "ixn_attr": "authType",
+            "enum_map": {"md5": "md5", "password": "password"},
+        },
+    }
+
+    _ORIGIN_TYPE = {
+        "origin_type": {
+            "ixn_attr": "routeOrigin",
+            "enum_map": {"internal": "internal", "external": "external"},
+        }
+    }
+
+    _REDISTRIBUTION_TYPE = {
+        "redistribution_type": {
+            "ixn_attr": "redistribution",
+            "enum_map": {"up": "up", "down": "down"},
+        }
+    }
+
     _BASIC = {
         "ipv4_te_router_id": "tERouterId",
         "hostname": "hostName",
@@ -10,6 +49,90 @@ class Isis(Base):
         "learned_lsp_filter": "discardLSPs",
     }
 
+    _ADVANCED = {
+        "enable_hello_padding": "enableHelloPadding",
+        "max_area_addresses": "maxAreaAddresses",
+        "area_addresses": "areaAddresses",
+        "lsp_refresh_rate": "lSPRefreshRate",
+        "lsp_lifetime": "lSPLifetime",
+        "psnp_interval": "pSNPInterval",
+        "csnp_interval": "cSNPInterval",
+        "max_lsp_size": "maxLSPSize",
+        "lsp_mgroup_min_trans_interval": "lSPorMGroupPDUMinTransmissionInterval",   # noqa
+        "enable_attached_bit": "attached",
+    }
+
+    _L1_SETTINGS = {
+        "priority": "level1Priority",
+        "hello_interval": "level1HelloInterval",
+        "dead_interval": "level1DeadInterval",
+    }
+
+    _L2_SETTINGS = {
+        "priority": "level2Priority",
+        "hello_interval": "level2HelloInterval",
+        "dead_interval": "level2DeadInterval",
+    }
+
+    # Array of objects
+    _MULTI_TOPOLOGY_IDS = {
+        "mt_id": "mtId",
+        "link_metric": "linkMetric",
+    }
+
+    # TBD
+    _TRAFFIC_ENGINEERING = {
+        "administrative_group": "administratorGroup",
+        "metric_level": "metricLevel",
+        "max_bandwith": "maxBandwidth",
+        "max_reservable_bandwidth": "maxReservableBandwidth",
+    }
+
+    _PRIORITY_BANDWIDTHS = {
+        "pb0": "bandwidthPriority0",
+        "pb1": "bandwidthPriority1",
+        "pb2": "bandwidthPriority2",
+        "pb3": "bandwidthPriority3",
+        "pb4": "bandwidthPriority4",
+        "pb5": "bandwidthPriority5",
+        "pb6": "bandwidthPriority6",
+        "pb7": "bandwidthPriority7",
+    }
+
+    _ADVANCED_INTERFACE = {
+        "auto_adjust_mtu": "autoAdjustMTU",
+        "auto_adjust_area": "autoAdjustArea",
+        "auto_adjust_supported_protocols": "autoAdjustSupportedProtocols",
+        "enable_3way_handshake": "enable3WayHandshake",
+        # "p2p_hellos_to_unicast_mac": "",
+    }
+
+    _LINK_PROTECTION = {
+        "extra_traffic": "extraTraffic",
+        "unprotected": "unprotected",
+        "shared": "shared",
+        "dedicated_1_to_1": "dedicatedOneToOne",
+        "dedicated_1_plus_1": "dedicatedOnePlusOne",
+        "enhanced": "enhanced",
+        "reserved_40": "reserved0x40",
+        "reserved_80": "reserved0x80",
+    }
+
+    # TBD
+    _ADJACENCY_SIDS = {}
+
+    _ROUTER_AUTH = {
+        "ignore_receive_md5": "ignoreReceiveMD5",
+        "area_auth": {
+            "ixn_attr": "areaAuthenticationType",
+            "enum_map": {"md5": "areaTransmitPasswordOrMD5Key", "password": "areaTransmitPasswordOrMD5Key"},    # noqa
+        },
+        "domain_auth": {
+            "ixn_attr": "domainAuthenticationType",
+            "enum_map": {"md5": "domainTransmitPasswordOrMD5Key", "password": "domainTransmitPasswordOrMD5Key"},    # noqa
+        },
+    }
+    
     def __init__(self, ngpf):
         super(Isis, self).__init__()
         self._ngpf = ngpf
