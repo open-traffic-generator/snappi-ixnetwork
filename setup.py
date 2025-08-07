84d6922f512c81ea52c817dafd20f2d6101ce0e9
Indrani Bhattacharya <70219648+indraniBh@users.noreply.github.com>
2025-08-07 14:57:38 +0530

    v1.33.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 import setuptools
 
 pkg_name = "snappi_ixnetwork"
-version = "1.33.0"
+version = "1.33.1"
 
 # read long description from readme.md
 base_dir = os.path.abspath(os.path.dirname(__file__))
