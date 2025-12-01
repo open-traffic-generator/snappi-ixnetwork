147fea5cfd9a705d165f85d2bc3aa8d8fad1fb81
Dipendu Ghosh <dipendughosh@gmail.com>
2025-12-01 17:16:56 +0530

    update icmp test case

--- tests/icmp/test_icmp_fields.py
+++ tests/icmp/test_icmp_fields.py
@@ -3,7 +3,6 @@ def test_icmp_fields(api, b2b_raw_config_vports, utils, tx_vport, rx_vport):
     """
     Configure three raw ICMP flows with:
     - fixed pattern for all ICMP fields
-    - list pattern for all ICMP fields
     - counter pattern for all ICMP fields
     Validate using utils.validate_config.
     """
@@ -17,121 +16,81 @@ def test_icmp_fields(api, b2b_raw_config_vports, utils, tx_vport, rx_vport):
 
     flow1 = b2b_raw_config_vports.flows[0]
     eth, ip, icmp1 = flow1.packet.ethernet().ipv4().icmp()
-    # icmp_echo1 = icmp1.echo
+    icmp_echo1 = icmp1.echo
 
     eth.src.value = "00:00:0a:00:00:01"
     eth.dst.value = "00:00:0b:00:00:02"
     ip.src.value = "10.1.1.1"
     ip.dst.value = "20.1.1.1"
 
-    icmp1.echo.type.value = icmp_type
-    icmp1.echo.code.value = icmp_code
-    icmp1.echo.checksum.custom = icmp_checksum
-    icmp1.echo.identifier.value = icmp_identifier
-    icmp1.echo.sequence_number.value = icmp_seq_num
+    icmp_echo1.type.value = icmp_type
+    icmp_echo1.code.value = icmp_code
+    icmp_echo1.checksum.custom = icmp_checksum
+    icmp_echo1.identifier.value = icmp_identifier
+    icmp_echo1.sequence_number.value = icmp_seq_num
 
-    # # List pattern
-    # flow2 = b2b_raw_config_vports.flows.flow(name="f2")[-1]
-    # flow2.tx_rx.port.tx_name = tx_vport.name
-    # flow2.tx_rx.port.rx_name = rx_vport.name
+    # Counter pattern
+    flow2 = b2b_raw_config_vports.flows.flow(name="f2")[-1]
+    flow2.tx_rx.port.tx_name = tx_vport.name
+    flow2.tx_rx.port.rx_name = rx_vport.name
 
-    # eth, ip, icmp2 = flow2.packet.ethernet().ipv4().icmp()
-    # icmp_echo2 = icmp2.echo
+    eth, ip, icmp2 = flow2.packet.ethernet().ipv4().icmp()
+    icmp_echo2 = icmp2.echo
+    eth.src.value = "00:00:0a:00:00:01"
+    eth.dst.value = "00:00:0b:00:00:02"
+    ip.src.value = "10.1.1.1"
+    ip.dst.value = "20.1.1.1"
 
-    # eth.src.value = "00:00:0a:00:00:01"
-    # eth.dst.value = "00:00:0b:00:00:02"
-    # ip.src.value = "10.1.1.1"
-    # ip.dst.value = "20.1.1.1"
+    icmp_echo2.type.increment.start = 0
+    icmp_echo2.type.increment.step = 1
+    icmp_echo2.type.increment.count = 10
 
-    # type_list = [0, 8, 11]
-    # code_list = [0, 1, 2]
-    # # checksum_list = [0x1111, 0x2222, 0x3333]
-    # identifier_list = [1, 2, 3]
-    # seq_list = [10, 20, 30]
+    icmp_echo2.code.increment.start = 0
+    icmp_echo2.code.increment.step = 1
+    icmp_echo2.code.increment.count = 5
 
-    # icmp_echo2.type.values = type_list
-    # icmp_echo2.code.values = code_list
-    # # icmp_echo2.checksum.custom = checksum_list
-    # icmp_echo2.identifier.values = identifier_list
-    # icmp_echo2.sequence_number.values = seq_list
+    icmp_echo2.identifier.increment.start = 0
+    icmp_echo2.identifier.increment.step = 1
+    icmp_echo2.identifier.increment.count = 10
 
-    # # Counter pattern
-    # flow3 = b2b_raw_config_vports.flows.flow(name="f3")[-1]
-    # flow3.tx_rx.port.tx_name = tx_vport.name
-    # flow3.tx_rx.port.rx_name = rx_vport.name
-
-    # eth, ip, icmp3 = flow3.packet.ethernet().ipv4().icmp()
-    # icmp_echo3 = icmp3.echo
-    # eth.src.value = "00:00:0a:00:00:01"
-    # eth.dst.value = "00:00:0b:00:00:02"
-    # ip.src.value = "10.1.1.1"
-    # ip.dst.value = "20.1.1.1"
-
-    # icmp_echo3.type.increment.start = 0
-    # icmp_echo3.type.increment.step = 1
-    # icmp_echo3.type.increment.count = 10
-
-    # icmp_echo3.code.increment.start = 0
-    # icmp_echo3.code.increment.step = 1
-    # icmp_echo3.code.increment.count = 5
-
-    # icmp_echo3.identifier.increment.start = 0
-    # icmp_echo3.identifier.increment.step = 1
-    # icmp_echo3.identifier.increment.count = 10
-
-    # icmp_echo3.sequence_number.increment.start = 0
-    # icmp_echo3.sequence_number.increment.step = 1
-    # icmp_echo3.sequence_number.increment.count = 10
+    icmp_echo2.sequence_number.increment.start = 0
+    icmp_echo2.sequence_number.increment.step = 1
+    icmp_echo2.sequence_number.increment.count = 10
 
     # Push config
     api.set_config(b2b_raw_config_vports)
 
-    # # Validate fixed
-    # f1_attrs = {
-    #     "MessageType": format(icmp_type, "x"),
-    #     "CodeValue": format(icmp_code, "x"),
-    #     "IcmpChecksum": format(icmp_checksum, "x"),
-    #     "Identifier": format(icmp_identifier, "x"),
-    #     "SequenceNumber": format(icmp_seq_num, "x"),
-    # }
-    # utils.validate_config(api, "f1", "icmpv2", **f1_attrs)
+    # Validate fixed
+    f1_attrs = {
+        "Message type": str(icmp_type),
+        "Code value": str(icmp_code),
+        "ICMP checksum": str(icmp_checksum),
+        "Identifier": str(icmp_identifier),
+        "Sequence number": str(icmp_seq_num),
+    }
+    utils.validate_config(api, "f1", "icmpv2", **f1_attrs)
 
-    # # Validate list
-    # f2_attrs = {
-    #     "MessageType": [format(v, "x") for v in type_list],
-    #     "CodeValue": [format(v, "x") for v in code_list],
-    #     # "IcmpChecksum": [format(v, "x") for v in checksum_list],
-    #     "Identifier": [format(v, "x") for v in identifier_list],
-    #     "SequenceNumber": [format(v, "x") for v in seq_list],
-    # }
-    # utils.validate_config(api, "f2", "icmp", **f2_attrs)
-
-    # # Validate counter
-    # f3_attrs = {
-    #     "MessageType": (
-    #         format(icmp_echo3.type.increment.start, "x"),
-    #         str(icmp_echo3.type.increment.step),
-    #         str(icmp_echo3.type.increment.count),
-    #     ),
-    #     "CodeValue": (
-    #         format(icmp_echo3.code.increment.start, "x"),
-    #         str(icmp_echo3.code.increment.step),
-    #         str(icmp_echo3.code.increment.count),
-    #     ),
-    #     "IcmpChecksum": (
-    #         format(icmp_echo3.checksum.increment.start, "x"),
-    #         str(icmp_echo3.checksum.increment.step),
-    #         str(icmp_echo3.checksum.increment.count),
-    #     ),
-    #     "Identifier": (
-    #         format(icmp_echo3.identifier.increment.start, "x"),
-    #         str(icmp_echo3.identifier.increment.step),
-    #         str(icmp_echo3.identifier.increment.count),
-    #     ),
-    #     "SequenceNumber": (
-    #         format(icmp_echo3.sequence_number.increment.start, "x"),
-    #         str(icmp_echo3.sequence_number.increment.step),
-    #         str(icmp_echo3.sequence_number.increment.count),
-    #     ),
-    # }
-    # utils.validate_config(api, "f3", "icmp", **f3_attrs)
+    # Validate counter
+    f2_attrs = {
+        "Message type": (
+            format(icmp_echo2.type.increment.start, "x"),
+            str(icmp_echo2.type.increment.step),
+            str(icmp_echo2.type.increment.count),
+        ),
+        "Code value": (
+            format(icmp_echo2.code.increment.start, "x"),
+            str(icmp_echo2.code.increment.step),
+            str(icmp_echo2.code.increment.count),
+        ),
+        "Identifier": (
+            format(icmp_echo2.identifier.increment.start, "x"),
+            str(icmp_echo2.identifier.increment.step),
+            str(icmp_echo2.identifier.increment.count),
+        ),
+        "Sequence number": (
+            format(icmp_echo2.sequence_number.increment.start, "x"),
+            str(icmp_echo2.sequence_number.increment.step),
+            str(icmp_echo2.sequence_number.increment.count),
+        ),
+    }
+    utils.validate_config(api, "f2", "icmpv2", **f2_attrs)
