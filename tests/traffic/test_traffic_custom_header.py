10ff093c48c4540853684a9fc4b8616cc2870740
Dipendu Ghosh <dipendughosh@gmail.com>
2025-12-10 15:34:33 +0530

    modify custom header to accomodate frame payload

--- tests/traffic/test_traffic_custom_header.py
+++ tests/traffic/test_traffic_custom_header.py
@@ -1,3 +1,6 @@
+import time
+import dpkt
+
 def test_traffic_custom_header(api, b2b_raw_config, utils):
     """
     Configure the devices on Tx and Rx Port.
@@ -25,6 +28,27 @@ def test_traffic_custom_header(api, b2b_raw_config, utils):
     flow.size.fixed = size
     flow.rate.percentage = 10
     flow.metrics.enable = True
+    flow.payload.choice = "fixed"
+    flow.payload.fixed.pattern = "4B6579736967687420546563686E6F6C6F67696573"
+    flow.payload.fixed.repeat = True
 
-    utils.start_traffic(api, b2b_raw_config)
+    api.set_config(b2b_raw_config)
 
+    utils.start_traffic(api, b2b_raw_config, start_capture=True)
+    time.sleep(10)
+    utils.stop_traffic(api, b2b_raw_config, stop_capture=True)
+
+    captures_ok(api, b2b_raw_config, packets)
+
+def captures_ok(api, cfg, packets):
+    pkt_count = 0
+
+    request = api.capture_request()
+    request.port_name = "rx"
+    pcap_bytes = api.get_capture(request)
+
+    for _, pkt in dpkt.pcapng.Reader(pcap_bytes):
+        eth = dpkt.ethernet.Ethernet(pkt)
+        pkt_count += 1
+                
+    assert pkt_count == packets
