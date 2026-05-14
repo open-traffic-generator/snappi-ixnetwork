503fcbd4e9266c35b95b8b5bda62139d4cc33063
suryjana <suryya.jana@keysight.com>
2026-05-14 23:06:30 +0530

    srv6-raw-traffic is pointing to snappi srv6-raw-traffic branch

--- do.py
+++ do.py
@@ -11,7 +11,7 @@ from email.mime.text import MIMEText
 
 global ixnexception
 
-SNAPPI_BRANCH=None
+SNAPPI_BRANCH="srv6-raw-traffic"
 def get_snappi_dev_branch():
     if SNAPPI_BRANCH is not None and SNAPPI_BRANCH != "":
         print(f"Test is using this snappi branch {SNAPPI_BRANCH}")
