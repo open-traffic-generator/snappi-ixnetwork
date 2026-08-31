99b550504ff6a575b458711f2319372a297a5e37
suryjana <suryya.jana@keysight.com>
2026-08-31 19:10:40 +0530

    merging from main and cleanup privets test files

--- do.py
+++ do.py
@@ -11,7 +11,7 @@ from email.mime.text import MIMEText
 
 global ixnexception
 
-SNAPPI_BRANCH="isis-srv6-review"
+SNAPPI_BRANCH=""  # Specify the branch name here if you want to install a specific branch of snappi
 def get_snappi_dev_branch():
     if SNAPPI_BRANCH is not None and SNAPPI_BRANCH != "":
         print(f"Test is using this snappi branch {SNAPPI_BRANCH}")
